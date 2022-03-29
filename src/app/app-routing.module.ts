e96dd99949798adeeb50f2312268135ea0a873ed
Janco Steenkamp <jancosteenkamp3@gmail.com>
2022-03-29 09:47:23 +0200

    Chapter1 Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Chapter1Component } from './chapter1/chapter1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'chapter1', component: Chapter1Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Chapter1Component } from './chapter1/chapter1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Chapter1Component
   ],
   imports: [
     BrowserModule,
